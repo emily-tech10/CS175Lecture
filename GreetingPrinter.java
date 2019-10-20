c49cb9c4e283843061ee6e6a156da74b9bde6e23
emily-tech10 <54564088+emily-tech10@users.noreply.github.com>
2019-10-19 20:13:40 -0400

    Add files via upload
    Chapter 1 Programming Tasks

---
+++ GreetingPrinter.java
@@ -0,0 +1,11 @@
+package ch1homework;
+
+public class GreetingPrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ NamePrinter.java
@@ -0,0 +1,18 @@
+package ch1homework;
+
+public class NamePrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println ("+-----+");
+		
+		System.out.println("|Emily|");
+		
+		System.out.println("+-----+");
+		
+		
+
+	}
+
+}
